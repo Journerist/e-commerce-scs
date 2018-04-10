73efd4e13fa3b362c8cb09bed263ba6dd761abd1
Sebastian Barthel <akrillo89@gmail.com>
2018-04-10 20:25:48 +0200

    start embedded mongodb in junit integration tests

--- product-catalog/src/test/java/com/example/commerce/catalog/infrastructure/DemoApplicationTests.java
+++ product-catalog/src/test/java/com/example/commerce/catalog/infrastructure/DemoApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.commerce.catalog.infrastructure;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +12,7 @@ import com.example.commerce.catalog.core.infrastructure.DemoApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {DemoApplication.class})
+@DataMongoTest
 public class DemoApplicationTests {
 
     @Test
